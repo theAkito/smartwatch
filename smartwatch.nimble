41f0d6c4d47a82754ef7f49d6084948746cf83a8
Akito <the@akito.ooo>
2020-08-01 15:49:21 +0200

    Overhaul Project Meta

--- smartwatch.nimble
+++ smartwatch.nimble
@@ -6,9 +6,28 @@ description   = "WebUI showing S.M.A.R.T. status of storage media on servers."
 license       = "GPL-3.0"
 srcDir        = "src"
 bin           = @["smartwatch"]
+skipDirs    = @["tasks"]
+skipFiles   = @["README.md"]
 
 
 # Dependencies
 
 requires "nim >= 1.2.4"
 requires "jester >= 0.4.3"
+
+# Tasks
+
+task test, "Run test.":
+  exec "nim cc -r tests/test.nim"
+task configure, "Configure project.":
+  exec "git fetch"
+  exec "git pull"
+  exec "git checkout master"
+  exec "git submodule update --init --recursive"
+  exec "git submodule update --recursive --remote"
+task build, "Build project.":
+  setCommand "c"
+task makecfg, "Create nim.cfg for optimized builds.":
+  exec "nim utils/cfg_optimized.nims"
+task clean, "Removes nim.cfg.":
+  exec "nim utils/cfg_clean.nims"
--- src/smartharvester.nim
+++ src/smartharvester.nim
@@ -1,18 +1,35 @@
 import
   os,
-  osproc
+  json,
+  tables
+from osproc import
+  execCmdEx
 from strutils import
   splitLines,
   contains
 from sequtils import
   keepIf,
   delete
+# from tables import
+  # getOrDefault
+# from json import
+#   parseJson,
+#   getStr,
+#   JsonNode
+
 
 const
-  smart = "/usr/sbin/smartctl"
-  smart_opts = " -a"
-  lsblk = "/bin/lsblk"
-  lsblk_opts = " -pilo KNAME"
+  debug_build {.booldefine.}: bool = false
+  smart_opts = " -a --json=c "
+  lsblk = "/bin/lsblk "
+  lsblk_opts = " -pilo KNAME "
+
+when debug_build == true:
+  const
+    smart = "/bin/bash -c /usr/bin/fakesmartctl "
+else:
+  const
+    smart = "/usr/sbin/smartctl "
 
 var
   blockdevices: seq[string]
@@ -30,7 +47,43 @@ proc getAllDevices*(): seq =
 proc getSmartData*(devices: seq[string]): bool =
   for dev in devices:
     writeLine(stdout, dev)
-    let (smart_data, err_code) = execCmdEx(smart & smart_opts & " " & dev)
-    writeLine(stdout, smart_data)
+    let
+      (raw_smart_data, err_code) = execCmdEx(smart & smart_opts & dev)
+      smart_data = raw_smart_data.parseJson
+      device_map = smart_data["device"].getFields
+      device_type = device_map.getOrDefault("type")
+      ata_smart_attributes = smart_data["ata_smart_attributes"].getFields
+      asa_table = ata_smart_attributes["table"].getElems
+      asa_t_id1 = asa_table[0].getFields
+      asa_t_id3 = asa_table[1].getFields
+      asa_t_id4 = asa_table[2].getFields
+      asa_t_id5 = asa_table[3].getFields
+      asa_t_id7 = asa_table[4].getFields
+      asa_t_id9 = asa_table[5].getFields
+      asa_t_id10 = asa_table[6].getFields
+      asa_t_id12 = asa_table[7].getFields
+      asa_t_id184 = asa_table[8].getFields
+      asa_t_id187 = asa_table[9].getFields
+      asa_t_id188 = asa_table[10].getFields
+      asa_t_id189 = asa_table[11].getFields
+      asa_t_id190 = asa_table[12].getFields
+      asa_t_id191 = asa_table[13].getFields
+      asa_t_id193 = asa_table[14].getFields
+      asa_t_id194 = asa_table[15].getFields
+      asa_t_id195 = asa_table[16].getFields
+      asa_t_id197 = asa_table[17].getFields
+      asa_t_id198 = asa_table[18].getFields
+      asa_t_id199 = asa_table[18].getFields
+      # Possible fields per SMART ID:
+      # id
+      # name
+      # value
+      # worst
+      # thresh
+      # when_failed
+      # flags (another JsonNode in itself)
+      # raw (another JsonNode in itself)
+    writeLine(stdout, device_type)
+    writeLine(stdout, asa_t_id1["name"])
 
 discard getSmartData(getAllDevices())
