9b7c5a19c101528083477d2cc6d0e52b87e85b0b
Akito <the@akito.ooo>
2020-08-03 19:18:00 +0200

    Add all possible SMART attributes

--- src/smartharvester.nim
+++ src/smartharvester.nim
@@ -45,6 +45,15 @@ proc hash*[A](x: seq[A]): Hash =
   result = !$result
 
 proc getSmartDataField(devices: seq[string], property: SmartProperty): OrderedTable[seq[string], seq[string]] =
+  # Possible fields per SMART ID:
+  # id
+  # name
+  # value
+  # worst
+  # thresh
+  # when_failed
+  # flags (another JsonNode in itself)
+  # raw (another JsonNode in itself)
   for dev in devices:
     let
       (raw_smart_data, err_code) = execCmdEx(smart & smart_opts & dev)
@@ -56,41 +65,198 @@ proc getSmartDataField(devices: seq[string], property: SmartProperty): OrderedTa
       serial_number  = smart_data["serial_number"].getStr
       ata_smart_attributes = smart_data["ata_smart_attributes"].getFields
       asa_table   = ata_smart_attributes["table"].getElems
-      st_id1   = asa_table[0].getFields
-      st_id3   = asa_table[1].getFields
-      st_id4   = asa_table[2].getFields
-      st_id5   = asa_table[3].getFields
-      st_id7   = asa_table[4].getFields
-      st_id9   = asa_table[5].getFields
-      st_id10  = asa_table[6].getFields
-      st_id12  = asa_table[7].getFields
-      st_id184 = asa_table[8].getFields
-      st_id187 = asa_table[9].getFields
-      st_id188 = asa_table[10].getFields
-      st_id189 = asa_table[11].getFields
-      st_id190 = asa_table[12].getFields
-      st_id191 = asa_table[13].getFields
-      st_id192 = asa_table[14].getFields
-      st_id193 = asa_table[15].getFields
-      st_id194 = asa_table[16].getFields
-      st_id195 = asa_table[17].getFields
-      st_id197 = asa_table[18].getFields
-      st_id198 = asa_table[19].getFields
-      st_id199 = asa_table[20].getFields
-      # Possible fields per SMART ID:
-      # id
-      # name
-      # value
-      # worst
-      # thresh
-      # when_failed
-      # flags (another JsonNode in itself)
-      # raw (another JsonNode in itself)
+      # st_id1   = asa_table[0].getFields
+      # st_id3   = asa_table[1].getFields
+      # st_id4   = asa_table[2].getFields
+      # st_id5   = asa_table[3].getFields
+      # st_id7   = asa_table[4].getFields
+      # st_id9   = asa_table[5].getFields
+      # st_id10  = asa_table[6].getFields
+      # st_id12  = asa_table[7].getFields
+      # st_id184 = asa_table[8].getFields
+      # st_id187 = asa_table[9].getFields
+      # st_id188 = asa_table[10].getFields
+      # st_id189 = asa_table[11].getFields
+      # st_id190 = asa_table[12].getFields
+      # st_id191 = asa_table[13].getFields
+      # st_id192 = asa_table[14].getFields
+      # st_id193 = asa_table[15].getFields
+      # st_id194 = asa_table[16].getFields
+      # st_id195 = asa_table[17].getFields
+      # st_id197 = asa_table[18].getFields
+      # st_id198 = asa_table[19].getFields
+      # st_id199 = asa_table[20].getFields
       device_info = @[
                         model_family,
                         model_name,
                         serial_number
                      ]
+    var
+      current_smart_elem: OrderedTable[string, JsonNode]
+      avail_smart_elems: seq[int]
+    for elem in asa_table:
+      current_smart_elem = elem.getFields
+      avail_smart_elems.add(current_smart_elem["id"].getInt)
+    for elem in avail_smart_elems:
+      case elem:
+        of 01:
+          discard
+        of 02:
+          discard
+        of 03:
+          discard
+        of 04:
+          discard
+        of 05:
+          discard
+        of 06:
+          discard
+        of 07:
+          discard
+        of 08:
+          discard
+        of 09:
+          discard
+        of 10:
+          discard
+        of 11:
+          discard
+        of 12:
+          discard
+        of 13:
+          discard
+        of 22:
+          discard
+        of 170:
+          discard
+        of 171:
+          discard
+        of 172:
+          discard
+        of 173:
+          discard
+        of 174:
+          discard
+        of 175:
+          discard
+        of 176:
+          discard
+        of 177:
+          discard
+        of 179:
+          discard
+        of 180:
+          discard
+        of 181:
+          discard
+        of 182:
+          discard
+        of 183:
+          discard
+        of 184:
+          discard
+        of 185:
+          discard
+        of 187:
+          discard
+        of 188:
+          discard
+        of 189:
+          discard
+        of 190:
+          discard
+        of 191:
+          discard
+        of 192:
+          discard
+        of 193:
+          discard
+        of 194:
+          discard
+        of 195:
+          discard
+        of 196:
+          discard
+        of 197:
+          discard
+        of 198:
+          discard
+        of 199:
+          discard
+        of 200:
+          discard
+        of 201:
+          discard
+        of 202:
+          discard
+        of 203:
+          discard
+        of 204:
+          discard
+        of 205:
+          discard
+        of 206:
+          discard
+        of 207:
+          discard
+        of 208:
+          discard
+        of 209:
+          discard
+        of 210:
+          discard
+        of 211:
+          discard
+        of 212:
+          discard
+        of 220:
+          discard
+        of 221:
+          discard
+        of 222:
+          discard
+        of 223:
+          discard
+        of 224:
+          discard
+        of 225:
+          discard
+        of 226:
+          discard
+        of 227:
+          discard
+        of 228:
+          discard
+        of 230:
+          discard
+        of 231:
+          discard
+        of 232:
+          discard
+        of 234:
+          discard
+        of 235:
+          discard
+        of 240:
+          discard
+        of 241:
+          discard
+        of 242:
+          discard
+        of 243:
+          discard
+        of 244:
+          discard
+        of 249:
+          discard
+        of 250:
+          discard
+        of 251:
+          discard
+        of 252:
+          discard
+        of 254:
+          discard
     case property:
       of RAW_READ_ERROR_RATE:
         let
@@ -285,4 +451,4 @@ proc getSmartDataAll*(devices: seq[string]): OrderedTable[seq[string], seq[seq[s
   return smart_all_perDevice
 #TODO DEBUG
 # echo getSmartDataAll(getAllDevices())
-echo getSmartDataAll(@["/dev/sda", "/dev/sdb"])
+# echo getSmartDataAll(@["/dev/sda", "/dev/sdb"])
