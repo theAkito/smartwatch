e88c680caaafcad1ee72bbfc1eeacb2957a03719
Akito <the@akito.ooo>
2020-08-05 20:48:48 +0200

    Clean up and fix table generation

--- src/smartwatch.nim
+++ src/smartwatch.nim
@@ -1,4 +1,3 @@
-# example.nim
 import
   smartharvest,
   htmlgen,
@@ -12,16 +11,9 @@ routes:
     redirect "/"
   get "/":
     let
+      smart_all: OrderedTable[seq[string], seq[seq[string]]] = getSmartDataAll(@["/dev/sda", "/dev/sdb"])
       smart_all1: OrderedTable[seq[string], seq[seq[string]]] = getSmartDataAll(@["/dev/sda"])
       smart_all2: OrderedTable[seq[string], seq[seq[string]]] = getSmartDataAll(@["/dev/sdb"])
-    proc createHtmlTable(smart_data: OrderedTable[seq[string], seq[seq[string]]]): string =
-      var
-        attr_list: seq[seq[string]]
-      for device_info, smart_attrs in smart_data:
-        attr_list = smart_data[device_info]
-        result.add createSmartHtmlTableTitleRow(device_info[0], device_info[1], device_info[2])
-        for attr in attr_list:
-          result.add createSmartHtmlTableRow(attr[0], attr[1], attr[2], attr[3])
     resp """<!DOCTYPE html>
 <head>
   <link rel="stylesheet" href="css/styles.css">
@@ -41,12 +33,7 @@ routes:
     <!--<li><a href='/test'>Test</a></li>-->
   </ul>
 </nav>""" &
-      br() &
-      """<table style="width:350px">""" &
-      createHtmlTable(smart_all1) &
-      """</table>""" &
-      """<table style="width:350px">""" &
-      createHtmlTable(smart_all2) &
-      """</table>"""
+      createHtmlTableList(smart_all) &
+      br()
   get "/test":
     resp "Test succeeded!"
--- src/tablegenerator.nim
+++ src/tablegenerator.nim
@@ -1,4 +1,7 @@
-import macros
+import
+  macros,
+  tables,
+  htmlgen
 
 macro createSmartHtmlTableRow*(arg: varargs[untyped]): untyped =
   arg.expectLen 4
@@ -28,3 +31,25 @@ macro createSmartHtmlTableTitleRow*(arg: varargs[untyped]): untyped =
     th("Thresh") &
     "</tr>" &
     "</thead>"
+
+proc createHtmlTableContent*(smart_data: OrderedTable[seq[string], seq[seq[string]]]): string =
+  var
+    attr_list: seq[seq[string]]
+  for device_info, smart_attrs in smart_data:
+    attr_list = smart_data[device_info]
+    result.add """<table style="width:350px">""" &
+               createSmartHtmlTableTitleRow(device_info[0], device_info[1], device_info[2])
+    for attr in attr_list:
+      result.add createSmartHtmlTableRow(attr[0], attr[1], attr[2], attr[3])
+    result.add """</table>"""
+
+macro createSmartHtmlTable*(arg: varargs[untyped]): untyped =
+  arg.expectLen 1
+  result = quote do:
+    """<table style="width:350px">""" &
+    createHtmlTableContent(`arg`) &
+    """</table>"""
+
+proc createHtmlTableList*(smart_data: OrderedTable[seq[string], seq[seq[string]]]): string =
+  for device_info, smart_attrs in smart_data:
+    result =  createSmartHtmlTable(smart_data)
